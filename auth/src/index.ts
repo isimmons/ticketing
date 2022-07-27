d38cbb60bef965ad7119ad13039984f09b318839
Ian Simmons <isimmons33@gmail.com>
2022-07-27 19:26:05 -0400

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import { app } from './app';
 import mongoose from 'mongoose';
 
 const start = async () => {
+  console.log('Starting up...');
+
   if (!process.env.JWT_KEY) throw new Error('Unable to verify key');
   if (!process.env.MONGO_URI)
     throw new Error('Unable to get correct db connection string');

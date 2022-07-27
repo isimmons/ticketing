4bdb2226fac630bcf69b2fd236cab5253f7020c0
Ian Simmons <isimmons33@gmail.com>
2022-07-27 19:41:54 -0400

    fixed failing test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -9,7 +9,7 @@ it('shows current user data when logged in', async () => {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
-    .expect(400);
+    .expect(200);
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
